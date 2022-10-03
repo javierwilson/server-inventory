f32e8296b50fdc3935d199bd7d1908757ce222c5
javier wilson <javier.wilson@viamo.io>
2022-10-03 01:36:04 -0600

    adding list of hosts and detail page

--- servers/models.py
+++ servers/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Server(models.Model):
     country = models.CharField(max_length=2)
+    hostname = models.CharField(max_length=16)
     dn_id = models.CharField(max_length=16)
     machine_id = models.IntegerField()
     created = models.DateTimeField('date created')
+    def __str__(self):
+        return self.hostname
